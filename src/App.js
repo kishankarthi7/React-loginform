256ab0a5d14797772ab46facb615c08171cd4e87
KISHAN L <kishan.karthi7@gmail.com>
2020-05-14 10:44:51 +0200

    adding data sorces page styles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 // import LoginPageHeader from './LoginPage/Components/LoginPageHeader';
 // import LoginFormPage from './LoginPage/Components/LoginFormPage';
 // import LoginPageFooter from './LoginPage/Components/LoginPageFooter';
-// import LoginPage from './LoginPage/Components/LoginPage';
+import LoginPage from './LoginPage/Components/LoginPage';
 // import PricePage from './Price/Components/PricePage';
-import DataSources from './Datasources/Components/DataSources';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <DataSources />
+        <LoginPage />
       </div>
     );
   }
--- src/Datasources/Components/DataSources.js
+++ src/Datasources/Components/DataSources.js
@@ -1,64 +1,183 @@
 import React, { Component } from 'react';
 import '../css/DataSources.css';
+import CardImage from '../images/architecture-auto-automobiles-bridge-210182.jpg';
+import CityAboveDark from '../images/CITY-ABOVE-DRK.jpg';
+import ChicagoImage from '../images/chicago-690364_1920.jpg';
 
 class DataSources extends Component {
   render() {
     return (
       <div className="Datasource-page">
-        <header>HEADER IMAGE</header>
+        <header>
+          <h1>Data Sources</h1>
+        </header>
         <section className="card-container">
-          <section className="column-one">
+          <section className="columns">
             <div className="card">
-              <div className="card-image">IMAGE 1</div>
-              <div className="card-body">column-one</div>
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">AGD 343</h3>
+                <p className="card-description">
+                  Volumne, speed, classification , occupancy
+                </p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 2</div>
-              <div className="card-body">column-one</div>
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">DeepBlue Sensor</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 3</div>
-              <div className="card-body">column-one</div>
-            </div>
-            <div className="card">
-              <div className="card-image">IMAGE 4</div>
-              <div className="card-body">column-one</div>
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">Network Cameras</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
           </section>
-          <section className="column-two">
+          <section className="columns">
             <div className="card">
-              <div className="card-image">IMAGE 1</div>
-              <div className="card-body">column-two</div>
+              <div className="card-image">
+                <img src={CityAboveDark} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">TrafiCam</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 2</div>
-              <div className="card-body">column-two</div>
+              <div className="card-image">
+                <img src={CityAboveDark} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">RTMS</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 3</div>
-              <div className="card-body">column-two</div>
-            </div>
-            <div className="card">
-              <div className="card-image">IMAGE 4</div>
-              <div className="card-body">column-two</div>
+              <div className="card-image">
+                <img src={CityAboveDark} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">Smart Sensor</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
           </section>
-          <section className="column-three">
+          <section className="columns">
             <div className="card">
-              <div className="card-image">IMAGE 1</div>
-              <div className="card-body">column-three</div>
+              <div className="card-image">
+                <img src={ChicagoImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">AutoScope</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 2</div>
-              <div className="card-body">column-three</div>
+              <div className="card-image">
+                <img src={ChicagoImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">FCD</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 3</div>
-              <div className="card-body">column-three</div>
+              <div className="card-image">
+                <img src={ChicagoImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">FCD</h3>
+                <p className="card-description">Description</p>
+              </div>
+            </div>
+          </section>
+          <section className="columns">
+            <div className="card">
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">FCD</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
             <div className="card">
-              <div className="card-image">IMAGE 4</div>
-              <div className="card-body">column-three</div>
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">Inductive Loops</h3>
+                <p className="card-description">Description</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">Radar</h3>
+                <p className="card-description">Description</p>
+              </div>
+            </div>
+          </section>
+          <section className="columns">
+            <div className="card">
+              <div className="card-image">
+                <img src={CardImage} alt="card header"></img>
+              </div>
+              <div className="card-body">
+                <div className="card-logo">
+                  <h2>LOGO</h2>
+                </div>
+                <h3 className="card-title">ANPR</h3>
+                <p className="card-description">Description</p>
+              </div>
             </div>
           </section>
         </section>
